6c4a0986acccd64676dbdc2a2eac4dd2b99d0d04
Payal Somawanshi <pihu.somwanshi03@gmail.com>
2025-05-17 16:54:01 +0530

    CORS Issue error - deployed

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ app = FastAPI()
 # Allow frontend (React app) to communicate with backend (FastAPI)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tourmaline-cocada-0a58a7.netlify.app/"],  # Specify React frontend URL
+    allow_origins=["*"],
+    # ["https://tourmaline-cocada-0a58a7.netlify.app"],  # Specify React frontend URL
     #allow_origins=["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
